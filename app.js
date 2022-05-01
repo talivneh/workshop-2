c48805efeb4ae94cca5235a9844bd5d6de4c37a7
Yonatan Levin <parahall@gmail.com>
2022-05-01 10:39:57 +0300

    The beginning

---
+++ app.js
@@ -0,0 +1,17 @@
+const endGameData = [{
+    name: "space-stone", avengers: ["captain-america", "iron-man"]
+}, {
+    name: "mind-stone", avengers: ["ant-man", "captain-america"]
+}, {
+    name: "reality-stone", avengers: ["rocket-raccoon", "thor"]
+}, {
+    name: "power-stone", avengers: ["war-machine", "nebula"]
+}, {
+    name: "time-stone", avengers: [{name: "hulk"}]
+}, {
+    name: "soul-stone", avengers: ["black-widow", "hawkeye"]
+}];
+
+const stones = document.querySelectorAll('.stone');
+const avengers = document.querySelectorAll('.avenger');
+const glove = document.querySelector('.infinity_glove');
